385dab17d2cf1ee8680048a3bf7055d7ef7227ec
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-03 18:39:35 -0500

    test: :grey_question: Change string connection

--- usersAdministration/readuser/src/config.js
+++ usersAdministration/readuser/src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/user";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/user";
