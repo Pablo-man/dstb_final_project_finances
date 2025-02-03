14d64f018c7c2ba31dffe6671cc7f626a11c5d68
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-03 17:48:50 -0500

    feat: :sparkles: Include DB connection for user domain
    Mongo DB database in ec2 instance

--- usersAdministration/deleteuser/src/config.js
+++ usersAdministration/deleteuser/src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/user";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0/user";
