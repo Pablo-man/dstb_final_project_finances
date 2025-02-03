8d8260149d4452354a96cc96a1178768b265e22d
Xavier Quishpe <quishpexavier27@gmail.com>
2025-02-02 19:03:31 -0500

    fix: :wrench: Test code without some code lines

--- usersAdministration/createuser/app.js
+++ usersAdministration/createuser/app.js
@@ -11,9 +11,10 @@ const app = express();
 // settings
 app.set("port", process.env.PORT || 3001);
 app.use((req, res, next)=>{
-    res.header(`Access-Control-Allow-Origin`, `*`);
+    res.header(`Access-Control-Allow-Origin`, [`http://localhost:5173`]);
     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    res.header(`Access-Control-Allow-Credentials`, `true`);
     next()
 })
 
