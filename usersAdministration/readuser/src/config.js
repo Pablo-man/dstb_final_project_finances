869fc7cd94da9fafeb0034a346a092a54cdc3f02
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-04 18:44:16 -0500

    test: :grey_question: Change string connection

--- usersAdministration/readuser/src/config.js
+++ usersAdministration/readuser/src/config.js
@@ -5,4 +5,4 @@ config();
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
 
-export const MONGODB_URI = process.env.MONGODB_URI || `mongod://${dbHost}:${dbPort}/user`;
+export const MONGODB_URI = process.env.MONGODB_URI || `mongod://db:27017/user`;
