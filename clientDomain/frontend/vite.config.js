06f1c98079a126ce09dece38621006adbc40e574
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-14 11:41:30 -0500

    fix: :wrench: Use other cors option

--- clientDomain/frontend/vite.config.js
+++ clientDomain/frontend/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      '3679-200-12-169-184.ngrok-free.app',
+    ],
+  },
   plugins: [
     react(),
     tailwindcss()
