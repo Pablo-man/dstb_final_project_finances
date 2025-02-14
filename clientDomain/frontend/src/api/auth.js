b653e8162ae70166a6d2aa3adfc6bb395d3253d5
Xavier Quishpe <quishpexavier27@gmail.com>
2025-02-14 03:33:28 -0500

    fix: :wrench: Changes bad secret name

--- clientDomain/frontend/src/api/auth.js
+++ clientDomain/frontend/src/api/auth.js
@@ -2,7 +2,6 @@ import axios from './axios'
 const API1 = 'http://34.192.69.145:300'
 const API2 = 'http://23.21.45.25:400'
 
-
 export const registerRequest = async (user) => axios.post(`${API1}1/`, user)
 
 export const signInRequest = async (user) => axios.post(`${API2}1/login`, user)
