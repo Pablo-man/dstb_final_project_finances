f09a9b8d70faf50e0603dbbeffe954891200a865
Xavier Quishpe <quishpexavier27@gmail.com>
2025-02-14 04:14:26 -0500

    fix: :wrench: Use other cors option

--- userDomain/createuser/app.js
+++ userDomain/createuser/app.js
@@ -4,22 +4,22 @@ import swaggerUI from "swagger-ui-express"
 import indexRoutes from "./src/routes/user.routes.js";
 import "./src/utils/mongoose.js"
 import specs from "./src/swagger/swagger.js";
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
 
 const app = express();
 
 // settings
-var corsOptions = {
-    origin: 'http://54.166.71.68:80',
-    optionsSuccessStatus: 200,
-    credentials: true
-  }
+app.set("port", process.env.PORT || 3001);
+app.use((req, res, next)=>{
+    res.header(`Access-Control-Allow-Origin`, [`http://54.166.71.68:80`]);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    res.header(`Access-Control-Allow-Credentials`, `true`);
+    next()
+})
+
 
 // middlewares
 app.use(morgan("dev"));
-app.use(cors(corsOptions))
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
