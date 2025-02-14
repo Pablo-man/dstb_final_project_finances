749d81d63ba526cd6713fa6bf820c9af49a271cb
Xavier Quishpe <quishpexavier27@gmail.com>
2025-02-14 03:22:54 -0500

    fix: :wrench: Changes local ips for new ec2 ips

--- userDomain/createuser/app.js
+++ userDomain/createuser/app.js
@@ -10,7 +10,7 @@ const app = express();
 // settings
 app.set("port", process.env.PORT || 3001);
 app.use((req, res, next)=>{
-    res.header(`Access-Control-Allow-Origin`, [`http://localhost:5173`]);
+    res.header(`Access-Control-Allow-Origin`, [`http://54.166.71.68:80`]);
     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
     res.header(`Access-Control-Allow-Credentials`, `true`);
