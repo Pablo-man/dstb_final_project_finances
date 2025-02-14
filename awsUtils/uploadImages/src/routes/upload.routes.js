52500bd5430b69d8661b2a71be2c6419f535eede
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-14 11:26:13 -0500

    fix: :wrench: Revert changes

--- awsUtils/uploadImages/src/routes/upload.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import { uploadImage } from '../controllers/s3.controllers.js';
-
-const router = new Router()
-
-router.post('/', uploadImage)
-
-export default router;
